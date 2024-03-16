1d4bef18d734114d2326420a4587b75ee63653ad
Азаркович Юлия <3tochki@gmail.com>
2024-03-17 01:28:11 +0300

    Бесконечная история, убраны повторы

--- src/ya/tasktracker/manager/InMemoryHistoryManager.java
+++ src/ya/tasktracker/manager/InMemoryHistoryManager.java
@@ -3,27 +3,58 @@ package ya.tasktracker.manager;
 import ya.tasktracker.task.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public final class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> taskList;
+    private final Map<Integer, Node> taskList;
+    private Node head;
+    private Node lastNode;
 
     public InMemoryHistoryManager() {
-        taskList = new ArrayList<>(10);
+        taskList = new HashMap<>();
     }
 
 
     @Override
     public void add(Task task) {
-        // если список заполнен полностью, удаляем первый элемент
-        if (taskList.size() == 10) {
-            taskList.remove(0);
+        if(taskList.containsKey(task.getId())){
+            removeNode(taskList.get(task.getId()));
         }
-        taskList.add(task);
+        if(head == null){
+            head = new Node(task);
+            lastNode = head;
+        }else {
+            lastNode.setNext(new Node(task, lastNode));
+            lastNode = lastNode.getNext();
+        }
+        taskList.put(task.getId(),lastNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node node = taskList.get(id);
+        removeNode(node);
     }
 
     @Override
     public ArrayList<Task> getHistory() {
+        ArrayList<Task> list = new ArrayList<>();
+        Node node = head;
+        while (node != null){
+            list.add(node.getTask());
+            node = node.getNext();
+        }
 
-        return new ArrayList<>(taskList);
+        return list;
+    }
+
+    private void removeNode(Node node){
+        node.getPrev().setNext(node.getNext());
+        if(node.getNext() != null){
+            node.getNext().setPrev(node.getPrev());
+        }
+        taskList.remove(node.getTask().getId());
     }
 }
