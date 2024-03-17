6727e1d8eef0e2d7f05abe712d2b958d1d1ddaa6
Азаркович Юлия <3tochki@gmail.com>
2024-03-17 22:32:16 +0300

    Дополнительное задание

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import ya.tasktracker.manager.HistoryManager;
+import ya.tasktracker.manager.Managers;
+import ya.tasktracker.manager.TaskManager;
 import ya.tasktracker.task.Epic;
 import ya.tasktracker.manager.InMemoryTaskManager;
 import ya.tasktracker.task.SubTask;
@@ -8,6 +11,40 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Поехали!");
 
-        System.out.println("Приехали");
+        Managers managers = new Managers();
+        TaskManager taskManager = managers.getDefault();
+        System.out.println(taskManager.getHistory());
+        int taskId1 = taskManager.createTask(new Task("первая задача"));
+        System.out.println(taskManager.getHistory());
+        int taskId2 = taskManager.createTask(new Task("вторая задача"));
+        System.out.println(taskManager.getHistory());
+        int epicId2 = taskManager.createEpic(new Epic("пустой эпик"));
+        System.out.println(taskManager.getHistory());
+        int epicId = taskManager.createEpic(new Epic("эпик с подзадачами"));
+        System.out.println(taskManager.getHistory());
+        Epic epic = taskManager.getEpic(epicId);
+        System.out.println(taskManager.getHistory());
+        epic.addSubTask(taskManager.getSubtask(taskManager.createSubTask(new SubTask("первая подзадача"))));
+        System.out.println(taskManager.getHistory());
+        epic.addSubTask(taskManager.getSubtask(taskManager.createSubTask(new SubTask("вторая подзадача"))));
+        System.out.println(taskManager.getHistory());
+        epic.addSubTask(taskManager.getSubtask(taskManager.createSubTask(new SubTask("третья подзадача"))));
+        System.out.println(taskManager.getHistory());
+        taskManager.getTask(taskId2);
+        System.out.println(taskManager.getHistory());
+        taskManager.getEpic(epicId2);
+        System.out.println(taskManager.getHistory());
+        taskManager.getEpic(epicId);
+        System.out.println(taskManager.getHistory());
+        taskManager.getSubtask(epic.getSubTask().get(2));
+        taskManager.getSubtask(epic.getSubTask().get(0));
+        System.out.println(taskManager.getHistory());
+        taskManager.deleteTask(taskId2);
+        System.out.println(taskManager.getHistory());
+        taskManager.deleteEpic(epicId);
+        System.out.println(taskManager.getHistory());
+
+
+
     }
 }
